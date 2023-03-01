440c4047a0b0f27a754d1461006c66d75ed804ec
Leandro Neckel <neckel.leandro@gmail.com>
2023-03-01 09:08:12 -0300

    docker cleaning script modification

--- clean_docker_files.cmd
+++ clean_docker_files.cmd
@@ -11,7 +11,7 @@ echo Removendo todas as imagens
 @REM for /f "tokens=*" %%i in ('docker images -aq') do (
 @REM     docker image rm -f %%i
 @REM )
-docker image rm asap_base_image
+docker image rm im_estudo_django
 @REM echo Executando limpeza de sistema docker
 @REM docker system prune -a -f
 echo Pronto
